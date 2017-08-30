2641ad4687ac66aee8f8a53371fa651a14a1e4a5
YoungHeaKim <dydgo9507@gmail.com>
2017-08-30 19:41:40 +0900

    use ejs to inclue css and ejs file

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express') // express를 연결해준다.
 const app = express() // app에 express를 실행하는 코드를 만든다.
 
+// express가 ejs를 템플릿 엔진으로 사용 가능하게 셋팅
+app.set('view engine', 'ejs')
+// static 라우트를 추가한다.
+app.use('/static', express.static('public'))
+
+// Route를 설정
 app.get('/', (req, res) => {
-  res.send('hello wold')
+  res.render('index.ejs')
 })
 
+// 서버연결시켜줌
 app.listen(5000, () => {
   console.log('listening')
 })
